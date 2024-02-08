b209d9a8005b0226a126f1c8f6e642a5e8503a06
Taky0ndon <char@za.rd>
2024-02-07 23:28:49 -0500

    used list comprehension instead of a traditional for loop for the conversion

--- src/main.py
+++ src/main.py
@@ -1,57 +1,9 @@
-DIT = "."
-DAH = "-"
+from conversions import text_to_morse_dict
 
 greeting = "Welcome to the Morse Code Generator!\
         Enter your text to convert to morse code here: "
-text_to_morse_dict = {
-        " ": " / ",
-        "?": "..--..",
-        "!": "-.-.--",
-        ".": ".-.-.-",
-        "$": "...-..-",
-
-        "A": ".-",
-        "B": "-...",
-        "C": "-.-.",
-        "D": "-..",
-        "E": ".",
-        "F": "..-.",
-        "G": "--.",
-        "H": "....",
-        "I": "..",
-        "J": ".---",
-        "K": "-.-",
-        "L": ".-..",
-        "M": "--",
-        "N": "-.",
-        "O": "---",
-        "P": ".--.",
-        "Q": "--.-",
-        "R": ".-.",
-        "S": "...",
-        "T": "-",
-        "U": "..-",
-        "V": "...-",
-        "W": ".--",
-        "X": ".--",
-        "Y": "-.--",
-        "Z": "--..",
-
-        "1":".----",
-        "2":"..---",
-        "3":"...--",
-        "4":"....-",
-        "5":".....",
-        "6":"-....",
-        "7":"--...",
-        "8":"---..",
-        "9":"----.",
-        "0":"-----",
-        }
 
 text_to_convert = input(greeting).upper()
-result = ""
-for char in text_to_convert:
-    result += f"{text_to_morse_dict[char]} "
+result = "".join([f"{text_to_morse_dict[char]} " for char in text_to_convert])
 
 print(f"Your morse code: {result}")
